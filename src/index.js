506647c2974e04a0e25e80443c563e4fd29ed497
sunan073 <msunan073@gmail.com>
2020-09-01 19:06:04 +0600

    redux added

--- src/index.js
+++ src/index.js
@@ -6,18 +6,25 @@ import {
   Route,
   withRouter,
 } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import firebase from "./config/firebase";
 import "semantic-ui-css/semantic.min.css";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         console.log(user);
+        this.props.setUser(user);
         this.props.history.push("/");
       }
     });
@@ -34,11 +41,25 @@ class Root extends React.Component {
   }
 }
 
-const RootWithRouter = withRouter(Root);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUser: (user) =>
+      dispatch({
+        type: "SET_USER",
+        payload: {
+          currentUser: user,
+        },
+      }),
+  };
+};
+
+const RootWithRouter = withRouter(connect(null, mapDispatchToProps)(Root));
 
 ReactDOM.render(
-  <Router>
-    <RootWithRouter />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithRouter />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
